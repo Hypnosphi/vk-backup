cdd7ee6c67f51c906656256f9a0ebe0861d28b70
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-04-30 03:16:24 +0300

    Trigger build on new post

--- functions/new-post.js
+++ functions/new-post.js
@@ -1,4 +1,11 @@
+const http = require('http')
+
 exports.handler = (event, context, callback) => {
+  http.request({
+    method: 'POST',
+    host: 'api.netlify.com',
+    path: `/build_hooks/${process.env.NETLIFY_HOOK_ID}`,
+  }).end()
   callback(null, {
     statusCode: 200,
     body: 'ok',
