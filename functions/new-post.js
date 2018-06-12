f3495b37459b2d49a2e6b90387adcd3f100585e4
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-06-12 14:49:45 +0300

    Add logging in lambda

--- functions/new-post.js
+++ functions/new-post.js
@@ -2,13 +2,13 @@ const http = require('http')
 
 exports.handler = (event, context, callback) => {
   const path = `/build_hooks/${process.env.NETLIFY_HOOK_ID}`
-  console.log('requesting:', process.env.NETLIFY_HOOK_ID)
+  console.log('requesting:', path)
   http.request({
     method: 'POST',
     host: 'api.netlify.com',
     path,
   }, res => {
-    if (res.statusCode !== 200) console.error(res.statusMessage)
+    if (res.statusCode !== 200) console.error(res.statusMessage, res.url)
     callback(null, {
       statusCode: 200,
       body: 'ok',
