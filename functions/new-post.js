0054af2c084929c2ef61aa9a4be81339081c52e1
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-06-12 15:37:16 +0300

    Use https in lambda

--- functions/new-post.js
+++ functions/new-post.js
@@ -1,18 +1,17 @@
-const http = require('http')
+const https = require('https')
 
 exports.handler = (event, context, callback) => {
   const path = `/build_hooks/${process.env.NETLIFY_HOOK_ID}`
   console.log('requesting:', path)
-  const req = http.request({
+  https.request({
     method: 'POST',
     host: 'api.netlify.com',
     path,
   }, res => {
-    if (res.statusCode !== 200) console.error(res.statusMessage, req.url)
+    if (res.statusCode !== 200) console.error(res.statusMessage)
     callback(null, {
       statusCode: 200,
       body: 'ok',
     })
-  })
-  req.end()
+  }).end()
 }
